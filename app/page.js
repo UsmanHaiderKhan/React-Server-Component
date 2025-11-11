0d123e75bda70e0999c4e1f2248eabd42681efc2
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-11-11 12:13:00 +0100

    ✨🎉 Initialize : Next.js project with basic setup
    Added initial Next.js app structure including global styles, layout, and main page. Configured project files such as .gitignore, jsconfig.json, next.config.mjs, and package.json. Included favicon and set up dependencies for Next.js, React, and ReactDOM.

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import ClientDemo from "@/app/components/ClientDemo";
+import RSCDemo from "@/app/components/RSCDemo";
+import DataFetchingDemo from "@/app/components/DataFetchingDemo";
+import ServerActionsDemo from "@/app/components/ServerActionsDemo";
+
+export default function Home() {
+  return (
+    <main>
+        <ServerActionsDemo/>
+        {/*<DataFetchingDemo/>*/}
+      {/*<ClientDemo >*/}
+      {/*    <RSCDemo/>*/}
+      {/*</ClientDemo>*/}
+    </main>
+  );
+}
